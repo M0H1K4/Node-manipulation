99f9126ed233ee2e16e92b070e4c171c3b3d5dad
M0H1K4N <nejer911@gmail.com>
2023-11-19 14:57:21 +0400

    ASYNCHRONOUS way of doing non blocking function

--- starter/index.js
+++ starter/index.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+//Blocking, SYNCHRONOUS way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// // შეიქმნა ახალი ფაილი ქვედა მოქმედებით
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written!");
 
-const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// შეიქმნა ახალი ფაილი ქვედა მოქმედებით
-fs.writeFileSync("./txt/output.txt", textOut);
-
-console.log("File Written!");
+//Non-Blocking, ASYNCHRONOUS way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2 ) => {
+        console.log(data2);
+      });
+});
+console.log("will read file!");
