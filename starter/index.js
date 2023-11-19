49702503fe53b2c03b5736eb1abbef89e07025c7
M0H1K4N <nejer911@gmail.com>
2023-11-20 01:16:19 +0400

    Template add with server

--- starter/index.js
+++ starter/index.js
@@ -34,20 +34,40 @@ const url = require("url");
 ////////////////////////////////////////////////////////////////
 
 /// SERVER 😎
-
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  /// Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is overviu simon");
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(tempOverview);
+
+    // Product Page
   } else if (pathName === "/product") {
     res.end("Aqana produqtebia simon !!!");
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
+    // Not Found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
