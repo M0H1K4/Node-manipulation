759454d7bd986cec8dcb21ef1c5871c1ba5e0dc2
M0H1K4N <nejer911@gmail.com>
2023-11-20 21:59:43 +0400

    add nodemon

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
+
 
 /////////////////////////////////////////////////////////////////
 /// FIles
@@ -49,7 +51,6 @@ const replaceTemplate = (temp, product) => {
 };
 
 const tempOverview = fs.readFileSync(
-
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
 );
@@ -66,23 +67,23 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-
-  const {query, pathname}=  url.parse(req.url, true)
-
+  const { query, pathname } = url.parse(req.url, true);
 
   /// Overview Page
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
-    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
     const output = tempOverview.replace("{%PRODUCT_CARD%}", cardsHtml);
     res.end(output);
 
     // Product Page
   } else if (pathname === "/product") {
     res.writeHead(200, { "Content-type": "text/html" });
-    const product = dataObj[query.id]
-    const output = replaceTemplate(tempProduct, product)
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
     // API
